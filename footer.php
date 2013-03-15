8f16b9765515a2e755fd34ff41a33d7770c1bd0b
Eduardo Lasso <elassol@hotmail.com>
2013-03-15 11:55:12 +0000

    Better header and Footer
    1 Html5 Tags for header and footer
    
    2 Better markup for the header
    
    3 Add Vcard to the footer
    
    	- Link to telefono
    	- mail
    	- link to cv
    
    Signed-off-by: Eduardo Lasso <elassol@hotmail.com>

--- footer.php
+++ footer.php
@@ -1,20 +1,45 @@
-	<div id="footer">
-		<div class="social-media-footer">
-			<p>Follow Us </p>
-    			<div class="sm-icons">
-        			<a class="facebook" target="_blank" href="https://www.facebook.com/pages/Lowe-Asia-Pacific/280511355293930">f</a>
-        			<a class="twitter" target="_blank"  href="https://twitter.com/loweasiapacific">f</a>
-        			<a class="youtube" target="_blank"  href="http://www.youtube.com/user/loweandpartners">f</a>
-        			<a class="linkedin" target="_blank"  href="http://www.linkedin.com/company/2282825?trk=tyah">f</a>
-        </div>
+           <footer id="footer">
 
-    	<p class="copyright">
-    		<strong>Copyright 2011 <?php bloginfo('name'); ?> | All Rights Reserved.</strong><br/>
-    		Designed by <a href="http://lassodesign.com">lassodesign</a>
-    	</p>  
-    </div>
+        		<div id="hcard-Eduardo-Lasso-Lajarin" class="vcard">
 
-</div>
- <?php wp_footer(); ?>
-</body>
+                    <a class="url fn n" href="http://lassodesign.com">
+                        <span class="given-name">Eduardo</span>
+                        <span class="family-name">Lasso</span>
+                    </a>
+
+                    <p>
+                        <strong>Mail:</strong> <a class="email" href="mailto:elassol@gmail.com">elassol@gmail.com</a><br/>
+                        <strong>Tel:</strong> <a class="tel" href="tel:+447789004876">+44 (0) 77 8900 4876</a>
+                    </p>
+
+                    
+
+                </div>
+
+
+                <div class="social-media-footer">
+        			<p>Follow Us </p>
+            		<div class="sm-icons">
+                		<a class="facebook" target="_blank" href="https://www.facebook.com/pages/Lowe-Asia-Pacific/280511355293930">f</a>
+                		<a class="twitter" target="_blank"  href="https://twitter.com/loweasiapacific">f</a>
+                		<a class="youtube" target="_blank"  href="http://www.youtube.com/user/loweandpartners">f</a>
+                		<a class="linkedin" target="_blank"  href="http://www.linkedin.com/company/2282825?trk=tyah">f</a>
+                    </div>
+                </div> 
+                
+                <div class="copyright-wrap">
+            	<p class="copyright">
+            		<strong>Copyright 2011 <?php bloginfo('name'); ?> | All Rights Reserved.</strong><br/>
+            		Designed by <a href="http://lassodesign.com">lassodesign</a>
+            	</p>  
+                </div>
+
+            </footer><!-- end footer -->
+
+        </div><!-- end #wrapper -->
+
+        <?php wp_footer(); ?>
+
+    </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,34 +1,39 @@
 <!doctype html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="description" content="Lassodesign Portfolio">  
-    <meta name="author" content="Eduardo Lasso">  
-    <meta name="viewport" content="width=device-width" />
-	<title><?php wp_title(''); ?><?php if (!(is_404()) && (is_single()) || (is_page()) || (is_archive())) { ?> &raquo; <?php } ?><?php bloginfo('name'); ?></title>
-    <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" /> 
-	
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
-<?php
-    wp_get_archives('type=monthly&format=link');
+    <head>
 
-    wp_head();
-?>
-</head>
-<body <?php body_class(); ?>>
+        <!--=== META TAGS ===-->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="description" content="Lassodesign Portfolio">  
+        <meta name="author" content="Eduardo Lasso">  
+        <meta name="viewport" content="width=device-width" />
+
+        <!--=== LINK TAGS ===-->
+    	
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" /> 
+    	
+        <!--=== TITLE ===--> 
+        <title><?php wp_title(''); ?><?php if (!(is_404()) && (is_single()) || (is_page()) || (is_archive())) { ?> &raquo; <?php } ?><?php bloginfo('name'); ?></title>
+        
+
+        <!--=== WP_HEAD() ===-->  
+        <?php wp_head(); ?>
+
+
+    </head>
+
+    <body <?php body_class(); ?>>
  
-<div id="wrapper">
-    <div id="header">
-        <h1 class="logo-header"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
-    </div>
-    <?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) ); ?>
+        <div id="wrapper">
+            <header id="header">
+                <h1 class="logo-header"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
+            </header><!-- end header -->
+            
+            <?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) ); ?>
